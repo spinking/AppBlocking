7f00a1a607a73930825b843c2ee6f80aa2b6dd12
Serge Bashkatov <spinking@mail.ru>
2020-07-08 20:03:03 +0400

    cleaned code

--- appBlockingLibrary/src/main/java/studio/eyesthetics/appblockinglibrary/BlockingBuilder.kt
+++ appBlockingLibrary/src/main/java/studio/eyesthetics/appblockinglibrary/BlockingBuilder.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
-import android.util.Log
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -21,15 +20,15 @@ class BlockingBuilder() {
     var compositeDisposable = CompositeDisposable()
 
     private var baseUrl = "https://google.com"
-    private var relativeUrl = "/"
+    private var endPoint = "/"
 
     fun setBaseUrl(baseUrl: String) : BlockingBuilder {
         this.baseUrl = baseUrl
         return this
     }
 
-    fun setRelativeUrl(relativeUrl: String) : BlockingBuilder {
-        this.relativeUrl = relativeUrl
+    fun setEndPoint(relativeUrl: String) : BlockingBuilder {
+        this.endPoint = relativeUrl
         return this
     }
 
@@ -52,21 +51,16 @@ class BlockingBuilder() {
 
 
         val disposable =
-            apiBlocking.blockingState(relativeUrl)
+            apiBlocking.blockingState(endPoint)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    {
-                        Log.d(
-                            "LOG_TAG",
-                            "message not blocking"
-                        )
-                    }
-                ) {
-                    Log.d("LOG_TAG", "message error + set content view + stop service")
+                .subscribe({
+
+                }, {
                     activity.setContentView(R.layout.blocking_layout)
                     stopServices(activity)
-                }
+                })
+
         compositeDisposable.add(disposable)
     }
 
@@ -74,15 +68,9 @@ class BlockingBuilder() {
         val am = activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         val rs =
             am.getRunningServices(50)
-        Log.d("LOG_TAG", "size " + rs.size)
         var message: String? = null
         for (i in rs.indices) {
-            val rsi =
-                rs[i] as ActivityManager.RunningServiceInfo
-            Log.d(
-                "LOG_TAG",
-                "Process " + rsi.process + " with component " + rsi.service.className
-            )
+            val rsi = rs[i] as ActivityManager.RunningServiceInfo
             message += rsi.process
             activity.stopService(
                 Intent(
@@ -98,87 +86,3 @@ class BlockingBuilder() {
         fun blockingState(@Url url: String) : Observable<Boolean>
     }
 }
-
-/*
-class AppBlockingModule {
-
-    var compositeDisposable = CompositeDisposable()
-
-    private var baseUrl = "https://google.com"
-    private var relativeUrl = "/"
-
-    fun setBaseUrl(baseUrl: String) {
-        this.baseUrl = baseUrl
-    }
-
-    fun setRelativeUrl(relativeUrl: String) {
-        this.relativeUrl = relativeUrl
-    }
-
-    fun blockingRequest(activity: Activity*/
-/*, useCase: Class<*>*//*
-) {
-
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BODY
-
-        val okHttpClient =
-            OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-        val apiBlocking =
-            Retrofit.Builder()
-                .baseUrl(baseUrl)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(okHttpClient)
-                .build()
-                .create(ApiBlocking::class.java) as ApiBlocking
-
-
-        val disposable =
-            apiBlocking.blockingState(relativeUrl)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    {
-                        Log.d(
-                            "LOG_TAG",
-                            "message not blocking"
-                        )
-                    }
-                ) {
-                    Log.d("LOG_TAG", "message error + set content view + stop service")
-                    activity.setContentView(R.layout.blocking_layout)
-                    stopServices(activity)
-                }
-        compositeDisposable.add(disposable)
-    }
-
-    private fun stopServices(activity: Activity) {
-        val am = activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val rs =
-            am.getRunningServices(50)
-        Log.d("LOG_TAG", "size " + rs.size)
-        var message: String? = null
-        for (i in rs.indices) {
-            val rsi =
-                rs[i] as ActivityManager.RunningServiceInfo
-            Log.d(
-                "LOG_TAG",
-                "Process " + rsi.process + " with component " + rsi.service.className
-            )
-            message += rsi.process
-            activity.stopService(
-                Intent(
-                    activity,
-                    (rs[i] as ActivityManager.RunningServiceInfo).javaClass
-                )
-            )
-        }
-    }
-
-    interface ApiBlocking {
-        @GET
-        fun blockingState(@Url url: String) : Observable<Boolean>
-    }
-}*/
